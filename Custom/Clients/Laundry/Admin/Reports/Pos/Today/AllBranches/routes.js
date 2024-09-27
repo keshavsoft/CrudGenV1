de61bed06600ad6fcfac414d7f619a07e60aa6c0
Pavansai <pavansai1151@gmail..com>
2024-09-27 17:03:39 +0530

    FactoryScan Endpoint

--- Custom/Clients/Laundry/Admin/Reports/Pos/Today/AllBranches/routes.js
+++ Custom/Clients/Laundry/Admin/Reports/Pos/Today/AllBranches/routes.js
@@ -6,10 +6,12 @@ import { router as QrCodes } from "./QrCodes/routes.js";
 import { router as Orders } from "./Orders/routes.js";
 import { router as ItemWise } from "./ItemWise/routes.js";
 import { router as ToFactory } from "./ToFactory/routes.js";
+import { router as FactoryScan } from "./FactoryScan/routes.js";
 
 router.use("/QrCodes", QrCodes);
 router.use("/Orders", Orders);
 router.use("/ItemWise", ItemWise);
 router.use("/ToFactory", ToFactory);
+router.use("/FactoryScan", FactoryScan);
 
 export { router };
