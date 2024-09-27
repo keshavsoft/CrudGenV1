997962961fc7bbf55b21ea6330cb3e035255255b
Shaik Rizwan <shaik.rizwan2002@gmail.com>
2024-09-27 16:12:49 +0530

    Orders Fixed

--- Custom/Clients/Laundry/Admin/Reports/Pos/Today/AllBranches/routes.js
+++ Custom/Clients/Laundry/Admin/Reports/Pos/Today/AllBranches/routes.js
@@ -3,7 +3,9 @@ import express from 'express';
 var router = express.Router();
 
 import { router as QrCodes } from './QrCodes/routes.js';
+import { router as Orders } from './Orders/routes.js';
 
 router.use('/QrCodes', QrCodes);
+router.use('/Orders', Orders);
 
 export { router };
