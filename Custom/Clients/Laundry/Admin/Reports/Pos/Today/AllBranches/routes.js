c4c5bec4b350c61e4fc854ecd0747fd0ecde1cb7
Pavansai <pavansai1151@gmail..com>
2024-09-27 17:20:18 +0530

    FactoryInwardReturn Endpoint

--- Custom/Clients/Laundry/Admin/Reports/Pos/Today/AllBranches/routes.js
+++ Custom/Clients/Laundry/Admin/Reports/Pos/Today/AllBranches/routes.js
@@ -7,11 +7,13 @@ import { router as Orders } from "./Orders/routes.js";
 import { router as ItemWise } from "./ItemWise/routes.js";
 import { router as ToFactory } from "./ToFactory/routes.js";
 import { router as FactoryScan } from "./FactoryScan/routes.js";
+import { router as FactoryInwardReturn } from "./FactoryInwardReturn/routes.js";
 
 router.use("/QrCodes", QrCodes);
 router.use("/Orders", Orders);
 router.use("/ItemWise", ItemWise);
 router.use("/ToFactory", ToFactory);
 router.use("/FactoryScan", FactoryScan);
+router.use("/FactoryInwardReturn", FactoryInwardReturn);
 
 export { router };
