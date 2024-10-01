2b2d5c1bb5bc8f4232d5ce0326a8469de3cf949a
Srivasu215 <sri.vasu215@gmail.com>
2024-10-01 12:57:16 +0530

    FACTORY ENTERY CANCEL

--- Custom/Clients/Laundry/Today/Orders/OrdersWithQrs/dals/GetFuncs/EntryFile.js
+++ Custom/Clients/Laundry/Today/Orders/OrdersWithQrs/dals/GetFuncs/EntryFile.js
@@ -1,8 +1,6 @@
 import { StartFunc as TodayOrdersWithQrs } from '../../kLowDb/ReadFileList/TodayOrdersWithQrs.js';
 
 let GetFuncs = ({ inBranch }) => {
-    console.log("pushpa");
-    
     return TodayOrdersWithQrs({ inBranch });
 };
 
