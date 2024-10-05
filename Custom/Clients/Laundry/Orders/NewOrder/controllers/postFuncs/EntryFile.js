c7dbdab7c6d4f89a90feb05621ba34da1a99395f
keshavsoft <nknnkumar@yahoo.com>
2024-10-05 10:59:51 +0530

    simplt

--- Custom/Clients/Laundry/Orders/NewOrder/controllers/postFuncs/EntryFile.js
+++ Custom/Clients/Laundry/Orders/NewOrder/controllers/postFuncs/EntryFile.js
@@ -6,6 +6,7 @@ let PostFunc = (req, res) => {
     let LocalBody = req.body;
 
     let LocalFromRepo = PostFuncRepo({ inBranch: LocalBranch, inPostBody: LocalBody });
+
     if (LocalFromRepo.KTF === false) {
         res.status(500).send(LocalFromRepo.KReason);
         return;
