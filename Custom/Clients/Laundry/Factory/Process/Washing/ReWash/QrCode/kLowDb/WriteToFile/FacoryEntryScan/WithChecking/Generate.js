a2be370f3b433dbbd8d1ae109bcbe75d729a58db
Srivasu215 <sri.vasu215@gmail.com>
2024-10-09 10:05:15 +0530

    CUSTOM

--- Custom/Clients/Laundry/Factory/Process/Washing/ReWash/QrCode/kLowDb/WriteToFile/FacoryEntryScan/WithChecking/Generate.js
+++ Custom/Clients/Laundry/Factory/Process/Washing/ReWash/QrCode/kLowDb/WriteToFile/FacoryEntryScan/WithChecking/Generate.js
@@ -2,17 +2,10 @@ const StartFunc = ({ inDataToInsert, inData, inColumns }) => {
     let LocalInData = inData;
     let LocalReturnData = { KTF: false, JSONFolderPath: "", CreatedLog: {} };
     let LocalDefalultKeys = LocalFuncDefalultKeys({ inColumns })
-    let LocalArrayPk = LocalInData.map(element => element.pk);
+    let MaxPk = Math.max(...LocalInData.map(el => el.pk).filter(el => el !== undefined).map(Number), 0) + 1;
+    let LoclaReWashCount = 1;
 
-    let LocalRemoveUndefined = LocalArrayPk.filter(function (element) {
-        return element !== undefined;
-    });
-
-    let numberArray = LocalRemoveUndefined.map(Number);
-
-    let MaxPk = (Math.max(...numberArray, 0) + 1);
-
-    LocalReturnData.InsertData = { ...LocalDefalultKeys, ...inDataToInsert, UuId: MaxPk, pk: MaxPk, DateTime: Timestamp() };
+    LocalReturnData.InsertData = { ...LocalDefalultKeys, ...inDataToInsert, ReWash: true, ReWashCount: LoclaReWashCount, UuId: MaxPk, pk: MaxPk, DateTime: Timestamp() };
     LocalReturnData.KTF = true;
     return LocalReturnData
 };
