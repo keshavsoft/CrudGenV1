4d89e185d78ae5efa1697c950571b47bad880064
keshavsoft <nknnkumar@yahoo.com>
2024-10-14 20:13:40 +0530

    bulk insert rest fixed

--- KCode/ForBat/CrudGenerator/ForBackendV4/ForRestClients/Bulk/PostEndPoints/EntryFile.js
+++ KCode/ForBat/CrudGenerator/ForBackendV4/ForRestClients/Bulk/PostEndPoints/EntryFile.js
@@ -7,6 +7,7 @@ const CommonBulk = "Bulk";
 
 import { StartFunc as home } from "./EndPointsContent/home.js";
 import { StartFunc as InsertWithCheck } from "./EndPointsContent/InsertWithCheck.js";
+import { StartFunc as MultiInsertWithCheck } from "./EndPointsContent/MultiInsertWithCheck.js";
 
 let StartFunc = ({ inTablesCollection, inTo, inConfigJson }) => {
     let LocalTypeName = `${CommonBulk}/restClients/PostEndPoints`;
@@ -28,41 +29,21 @@ let StartFunc = ({ inTablesCollection, inTo, inConfigJson }) => {
             inConfigJson,
             inTableNameWithExtension: element.name
         });
+
         InsertWithCheck({
             inFrom: `${process.env.PORT}/${LocalTo}/${LoopInsideFileName}`,
             inTo: `${LocalFilePath}`,
             inConfigJson,
             inTableNameWithExtension: element.name
         });
+
+        MultiInsertWithCheck({
+            inFrom: `${process.env.PORT}/${LocalTo}/${LoopInsideFileName}`,
+            inTo: `${LocalFilePath}`,
+            inConfigJson,
+            inTableNameWithExtension: element.name
+        });
     });
-};
-
-const LocalFuncFilterDataFromBody = ({ inFrom, inTo, inConfigJson, inTableNameWithExtension }) => {
-    let LocalFileData = `POST http://localhost:${inFrom}/${CommonBulk}\r\n`;
-    LocalFileData += `Content-Type: application/json\r\n`;
-
-    let LocalColumnsSchema = LocalFuncGetTableSchema({
-        inConfigJson,
-        inTableNameWithExtension
-    });
-
-    fs.writeFileSync(`${inTo}/home.http`, `${LocalFileData}\r\n${JSON.stringify(LocalColumnsSchema)}`);
-    fs.writeFileSync(`${inTo}/InsertWithCheck.http`, `${LocalFileData}\r\n${JSON.stringify(LocalColumnsSchema)}`);
-};
-
-const LocalFuncGetTableSchema = ({ inConfigJson, inTableNameWithExtension }) => {
-    let LocalChildren = inConfigJson.jsonConfig.tableAndColumns.children;
-    let LocalColumnsSchemaToReturn = {};
-
-    let LocalColumnsSchema = LocalChildren.find(element => {
-        return element.name === inTableNameWithExtension;
-    });
-
-    for (const [key, value] of Object.entries(LocalColumnsSchema.fileData)) {
-        LocalColumnsSchemaToReturn[key] = "";
-    };
-
-    return LocalColumnsSchemaToReturn;
 };
 
 export { StartFunc };
