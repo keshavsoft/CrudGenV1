1d41fadcef1c6eceab2fa451b7781ed2ec80b7df
pushpadamalanka82 <y*********@gmail.com>
2024-10-15 11:39:09 +0530

    FilterFactory endpoint

--- Custom/Clients/Laundry/Factory/Inward/Scan/Voucher/repos/getFuncs/EntryFile.js
+++ Custom/Clients/Laundry/Factory/Inward/Scan/Voucher/repos/getFuncs/EntryFile.js
@@ -1,10 +1,15 @@
 import {
     GetFunc as GetFuncDal,
+    GetFilterFactoryFunc as GetFilterFactoryFuncDal,
     GetQrStatusFunc as GetQrStatusFuncDal
 } from '../../dals/getFuncs/EntryFile.js';
 
-let GetFunc = ({ inFactory }) => {
-    return GetFuncDal({ inFactory });
+let GetFunc = () => {
+    return GetFuncDal();
+};
+
+let GetFilterFactoryFunc = ({ inFactory }) => {
+    return GetFilterFactoryFuncDal({ inFactory });
 };
 
 let GetQrStatusFunc = ({ inFactory }) => {
@@ -12,5 +17,5 @@ let GetQrStatusFunc = ({ inFactory }) => {
 };
 
 export {
-    GetFunc, GetQrStatusFunc
+    GetFunc, GetFilterFactoryFunc, GetQrStatusFunc
 };
--- Custom/Clients/Laundry/Factory/Inward/Scan/Voucher/routes/getFuncs/EntryFile.js
+++ Custom/Clients/Laundry/Factory/Inward/Scan/Voucher/routes/getFuncs/EntryFile.js
@@ -3,12 +3,12 @@ import express from 'express';
 var router = express.Router();
 
 import {
-    GetFunc, GetQrStatusFunc
+    GetFunc, GetFilterFactoryFunc, GetQrStatusFunc
 }
     from '../../controllers/getFuncs/EntryFile.js';
 
 router.get('/', GetFunc);
-router.get('/:inFactory', GetFilterFactoryFunc);
+router.get('FilterFactory/:inFactory', GetFilterFactoryFunc);
 router.get('/QrStatus/:inFactory', GetQrStatusFunc);
 
 export { router };
