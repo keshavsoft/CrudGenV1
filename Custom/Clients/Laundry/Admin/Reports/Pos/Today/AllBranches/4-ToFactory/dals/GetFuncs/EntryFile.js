8bd1866b454c84c73a1e2ff3c20594669168af60
keshavsoft <nknnkumar@yahoo.com>
2024-10-16 10:29:20 +0530

    admin started

--- Custom/Clients/Laundry/Admin/Reports/Pos/Today/AllBranches/4-ToFactory/dals/GetFuncs/EntryFile.js
+++ Custom/Clients/Laundry/Admin/Reports/Pos/Today/AllBranches/4-ToFactory/dals/GetFuncs/EntryFile.js
@@ -1,8 +1,7 @@
-// import { StartFunc as ToadyQrAllReports } from '../../kLowDb/ReadFileList/ToadyQrAllReports.js';
+import { StartFunc as applyFilter } from '../../kLowDb/ReadFromFile/applyFilter.js';
 
 let GetAllFuncs = () => {
-    return "From Dal"
-    // return ToadyQrAllReports({ inBranch });
+    return applyFilter();
 };
 
 export {
