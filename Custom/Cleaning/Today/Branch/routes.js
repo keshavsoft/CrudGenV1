49a27aae6b13128ff112fc4aa338d3dc3d71667b
keshavsoft <nknnkumar@yahoo.com>
2024-10-18 12:07:29 +0530

    custom today fixed

--- Custom/Cleaning/Today/Branch/routes.js
+++ Custom/Cleaning/Today/Branch/routes.js
@@ -2,7 +2,8 @@ import express from 'express';
 
 var router = express.Router();
 
-import { router as routerFromQrcodes } from './Qrcodes/routes.js';
+
+import { router as routerFromQrcodes } from './QrCodes/routes.js';
 
 router.use('/Qrcodes', routerFromQrcodes);
 
